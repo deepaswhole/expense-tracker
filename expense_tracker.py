6bcf2de087b4c6717aee021bada8264601753dd4
deepaswhole <lanwong0419@gmail.com>
2024-12-30 20:45:38 +0800

    Add functionality to search expenses by category

--- expense_tracker.py
+++ expense_tracker.py
@@ -11,4 +11,12 @@ def view_expenses():
     else:
         for i, expense in enumerate(expenses, 1):
             print(f"{i}. Category : {expense['category']}, Amount: ${expense['amount']}, Description: {expense['description']}")
-        
+
+def search_expenses(category):
+    found = [expense for expense in expenses if expense['category'].lower() == category.lower()]
+    if found:
+        for expense in found:
+            print(f"Category: {expense['category']}, Amount: ${expense["amount"]}, Description: {expense['description']}")
+        else:
+            print(f"No expenses found in category: {category}")
+            
